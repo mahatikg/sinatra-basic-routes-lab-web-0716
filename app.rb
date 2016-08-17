f98ab5d93ae2eee678481d4b9d6f604c3bc70d32
Mahati Gollamudi <github email address>
2016-08-17 01:33:23 -0400

    Add Three Get requests

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Mahati!"
+  end
+
+  get '/hometown' do
+    "My hometown is Reston, VA!"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is all things Beyonce"
+  end
+
 end
